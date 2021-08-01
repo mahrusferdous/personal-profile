ff6e725d23c10bcc3d7921f9ca606d2c3b4563aa
mahru <mahrusferdous@gmail.com>
2021-08-01 19:17:51 -0400

    added js effect

--- js/index.js
+++ js/index.js
@@ -47,3 +47,45 @@ tl.fromTo(
     { yPercent: -500, duration: 0 },
     { yPercent: 0, duration: 1 }
 );
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".description",
+        start: "top center",
+        onEnter: () => $(".descrip-class").addClass("show"),
+    },
+});
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".i-do",
+        start: "top center",
+        onEnter: () => {
+            window.setTimeout(() => {
+                $(".passion-class1").addClass("show");
+            });
+            window.setTimeout(() => {
+                $(".passion-class2").addClass("show");
+            }, 500);
+            window.setTimeout(() => {
+                $(".passion-class3").addClass("show");
+            }, 1000);
+        },
+    },
+});
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".experience",
+        start: "top 35%",
+        onEnter: () => $(".logo-row").addClass("show"),
+    },
+});
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".projects",
+        start: "top center",
+        onEnter: () => $(".project-img").addClass("show"),
+    },
+});
