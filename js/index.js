a95f18a3eaf82231169692d21266b4faf4532c7f
mahru <mahrusferdous@gmail.com>
2021-08-26 22:37:05 -0400

    minor bug fixed

--- js/index.js
+++ js/index.js
@@ -2,25 +2,19 @@ const toggleBtn = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navlink")[0];
 
 toggleBtn.addEventListener("click", function () {
-    if (
-        document.body.scrollTop > 30 ||
-        document.documentElement.scrollTop > 30
-    ) {
-        navbarLinks.classList.toggle("active");
-    }
+    // if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    navbarLinks.classList.toggle("active");
+    // }
 });
 
 window.onscroll = function () {
-    if (
-        document.body.scrollTop > 30 ||
-        document.documentElement.scrollTop > 30
-    ) {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
         $(".nav-bar-nav").addClass("nav-fixed");
         $(".toggle-button").addClass("toggle-fix");
     } else {
         $(".nav-bar-nav").removeClass("nav-fixed");
         $(".toggle-button").removeClass("toggle-fix");
-        $(".navlink").removeClass("active");
+        // $(".navlink").removeClass("active");
     }
 };
 
@@ -42,11 +36,7 @@ const tl = gsap.timeline({
     },
 });
 
-tl.fromTo(
-    ".envelope",
-    { yPercent: -500, duration: 0 },
-    { yPercent: 0, duration: 1 }
-);
+tl.fromTo(".envelope", { yPercent: -500, duration: 0 }, { yPercent: 0, duration: 1 });
 
 gsap.timeline({
     scrollTrigger: {
